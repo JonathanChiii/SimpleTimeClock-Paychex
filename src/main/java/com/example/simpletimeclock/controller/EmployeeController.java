d212a0a3d4457563f79ef7b2e961c67d7a5aa9c8
Jonathan PC <chiqingyuan@hotmail.com>
2023-02-06 01:43:49 -0500

    Finished setting up session

--- src/main/java/com/example/simpletimeclock/controller/EmployeeController.java
+++ src/main/java/com/example/simpletimeclock/controller/EmployeeController.java
@@ -4,12 +4,17 @@ import com.example.simpletimeclock.dto.LoginRequest;
 import com.example.simpletimeclock.model.Employee;
 import com.example.simpletimeclock.repository.EmployeeRepository;
 import com.example.simpletimeclock.repository.ShiftRepository;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import lombok.extern.java.Log;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 @Controller
@@ -31,27 +36,45 @@ public class EmployeeController {
         model.addAttribute("employee", e);
         return "postRegister";
     }
+
+    // Session Management
+    // https://www.javainuse.com/spring/springboot_session
     @GetMapping("/login")
-    public String loginPage(@RequestParam Optional<String> id, Model model){
+    public String loginPage(@RequestParam Optional<String> id, HttpSession session, Model model){
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) session.getAttribute("SESSION_MESSAGES");
+        if(messages == null){ messages = new ArrayList<>();}
+        model.addAttribute("sessionMessages", messages);
+
         LoginRequest dto = new LoginRequest();
         if(id.isPresent()){
             dto.setId(id.get());
         }
         model.addAttribute("employee", dto);
+
         return "login";
     }
 
     @PostMapping("/login")
-    public String login(@ModelAttribute("employee")LoginRequest loginRequest){
-        Long id;
-//        try{
-//            id = Long.valueOf(loginRequest.getId());
-//            System.out.println(loginRequest.getId());
-//        }catch(Exception e){
-//            System.out.println("Exception occured");
-//            return "error";
-//        }
-        // need to grant http session
+    public String login(@ModelAttribute("employee")LoginRequest loginRequest, HttpServletRequest request){
+        Long id = Long.valueOf(loginRequest.getId());
+        Employee employee = employeeRepository.getReferenceById(id);
+        System.out.println("request username");
+        System.out.println(request.getAttribute("username"));
+        System.out.println(request.getAttribute("password"));
+        if(employee == null || !employee.getPassword().equals(loginRequest.getPassword())){
+            return "error";
+        }
+        @SuppressWarnings("unchecked")
+        List<String> messages = (List<String>) request.getSession().getAttribute("MY_SESSION_MESSAGES");
+        if (messages == null) {
+            messages = new ArrayList<>();
+            request.getSession().setAttribute("username", employee.getUsername());
+            request.getSession().setAttribute("id", employee.getId());
+        }
+        messages.add(employee.toString());
+        System.out.println(employee.toString());
+        request.getSession().setAttribute("SESSION_MESSAGES", messages);
         return "redirect:/index";
     }
 
--- src/main/java/com/example/simpletimeclock/controller/ShiftController.java
+++ src/main/java/com/example/simpletimeclock/controller/ShiftController.java
@@ -1,20 +1,35 @@
 package com.example.simpletimeclock.controller;
 
+import com.example.simpletimeclock.model.Employee;
 import com.example.simpletimeclock.repository.BreakRepository;
+import com.example.simpletimeclock.repository.EmployeeRepository;
 import com.example.simpletimeclock.repository.ShiftRepository;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Optional;
 
 @Controller
 public class ShiftController {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
     @Autowired
     ShiftRepository shiftRepository;
     @Autowired
     BreakRepository breakRepository;
 
     @GetMapping("/index")
-    public String index(){
+    public String index(Model model, HttpServletRequest request){
+        String username = request.getSession().getAttribute("username").toString();
+        Long id = Long.valueOf(request.getSession().getAttribute("id").toString());
+        System.out.println("Session User:" + username);
+        Employee employee = employeeRepository.getReferenceById(id);
+        model.addAttribute("employee", employee);
         return "index";
     }
 
